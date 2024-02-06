ca29be393ea9df08560185e59d0695eb5c5353d7
elmar <elmarcera@gmail.com>
2024-02-06 15:10:44 +0800

    Thinking About State and Lifting State Up - start

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@
 //   { id: 3, description: "Charger", quantity: 12, packed: false },
 // ];
 
-import { useState } from "react";
+// import { useState } from "react";
 
 // export default function App() {
 //   return (
@@ -153,36 +153,270 @@ import { useState } from "react";
 //   );
 // }
 
-/////////////////////////////////////////////////////////////
-// Controlled Elements
-/////////////////////////////////////////////////////////////
+// /////////////////////////////////////////////////////////////
+// // Controlled Elements
+// /////////////////////////////////////////////////////////////
 
-// Let's now learn about yet another
-// fundamental React concept which is controlled elements.
-// So let's take a look what they are
-// and how we use controlled elements
-// when working with forms in React.
-// So to start, what we need to understand is that by default
-// these input fields like this input and also this select
-// they maintain their own state inside the DOM.
-// So basically inside the HTML element itself.
-// Now this makes it hard to read their values
-// and it also leaves this state right here in the DOM
-// which for many reasons is not ideal.
-// So in React, we usually like to keep all this state
-// in just one central place.
-// So inside the React application and not inside the DOM.
-// And so in order to do that we use a technique
-// called controlled elements.
-// And so with this technique
-// it is React who controls and owns the state
-// of these input fields and no longer the DOM.
-// So since we want to now keep this data
-// inside the application, what that means
-// is that we need some state, right?
-// Because that form data of course changes over time
-// and we also want to maintain our application
-// in sync with it.
+// // Let's now learn about yet another
+// // fundamental React concept which is controlled elements.
+// // So let's take a look what they are
+// // and how we use controlled elements
+// // when working with forms in React.
+// // So to start, what we need to understand is that by default
+// // these input fields like this input and also this select
+// // they maintain their own state inside the DOM.
+// // So basically inside the HTML element itself.
+// // Now this makes it hard to read their values
+// // and it also leaves this state right here in the DOM
+// // which for many reasons is not ideal.
+// // So in React, we usually like to keep all this state
+// // in just one central place.
+// // So inside the React application and not inside the DOM.
+// // And so in order to do that we use a technique
+// // called controlled elements.
+// // And so with this technique
+// // it is React who controls and owns the state
+// // of these input fields and no longer the DOM.
+// // So since we want to now keep this data
+// // inside the application, what that means
+// // is that we need some state, right?
+// // Because that form data of course changes over time
+// // and we also want to maintain our application
+// // in sync with it.
+
+// const initialItems = [
+//   { id: 1, description: "Passports", quantity: 2, packed: false },
+//   { id: 2, description: "Socks", quantity: 12, packed: true },
+//   { id: 3, description: "Charger", quantity: 12, packed: false },
+// ];
+
+// export default function App() {
+//   return (
+//     <div className="app">
+//       <Logo />
+//       <Form />
+//       <PackingList />
+//       <Stats />
+//     </div>
+//   );
+// }
+
+// function Logo() {
+//   return <h1>🌴 Far Away 💼</h1>;
+// }
+
+// function Form() {
+//   //   So in order to implement the controlled elements technique,
+//   // we follow three steps.
+//   // First we create a piece of state.
+//   // So let's start with that
+//   // and we will start here with this actual input element.
+//   // So with this text field right there.
+//   const [description, setDescription] = useState("");
+
+//   const [quantity, setQuantity] = useState(1);
+
+//   function handleSubmit(e) {
+//     e.preventDefault();
+
+//     if (!description) return;
+
+//     // And then we also need an ID.
+//     // Now we could use some library here to generate that ID
+//     // but let's do it quick and dirty here, just with date.now.
+//     // So that should just work here in this case.
+//     const newItem = { description, quantity, packed: false, id: Date.now() };
+//     console.log(newItem);
+//     //     So with this we learned how we get now this data
+//     // out of the form.
+
+//     //     And now finally, usually when we submit a form
+//     // then afterwards, once that submission is done
+//     // the form should go back to its initial state.
+//     // So let's also do that.
+//     // And for that we can simply use our setter functions, right?
+//     // And so that's the beauty
+//     // of React being in charge of the form
+//     // because now all we have to do is to update the state
+//     // and then this enables React to automatically keep this state
+//     // in sync with these form elements.
+//     // So basically that's the whole idea
+//     // of the controlled elements.
+//     setDescription("");
+//     setQuantity(1);
+
+//     //     So that worked really, really nicely.
+//     // So this form is now a lot more real world.
+//     // It's back to its initial state.
+//     // And then down here we get the data.
+//     // But now what do we do with this data?
+//     // So at some point we will want to actually render
+//     // this new object here into the user interface.
+//     // So right here into this packing list, right?
+//     // So how do you think we will do that?
+//     // So how can we get this new state?
+//     // So basically this new object that we just created
+//     // into this list.
+//     // And just as a reminder, this list,
+//     // well, it's even easier to see in the componentry.
+//     // So we have the form, and here is the list.
+//     // So we want to get the data from this form
+//     // into this packing list right here.
+//     // So do you think that we could do that with props?
+//     // Well, not really, right?
+//     // Because these are sibling components.
+//     // The form is not a parent component of the packing list
+//     // and therefore we cannot pass props from form
+//     // into the packing list, right?
+//     // So because data can only flow down the tree
+//     // but not up or sideways.
+//     // So that was one of the important things
+//     // that we learned about props, remember that?
+//     // So therefore, if we cannot use props in a simple way
+//     // we need to find another solution.
+//     // And so this is where we really need to start
+//     // thinking more about state and state management.
+//     // But since this is so important as a React developer,
+//     // I created an entire separate section
+//     // about thinking in React and that section is up next.
+//     // So what we're gonna do now is to take a break
+//     // in this application and finish this section
+//     // with a brief summary and a challenge to consolidate
+//     // our knowledge about how to use state in React.
+//     // And then after that, we will come back
+//     // here to this application
+//     // and then we will really make it work.
+//     // So then we will be able to pass data
+//     // basically from the form into the list.
+//     // So that's going to be a lot of fun.
+//     // So make sure to finish this section
+//     // and then right afterwards,
+//     // let's keep going with this application.
+//   }
+
+//   //   So just to quickly recap,
+//   // the technique of controlled elements
+//   // basically consists of three steps.
+//   // So we define a piece of state, like this description here
+//   // then we use that piece of state
+//   // on the element that we want to control.
+//   // So we basically force the element to always take the value
+//   // of this state variable.
+//   // And then finally, of course
+//   // we need to update that state variable.
+//   // And we do so here with the on change handler
+//   // where we then set the description
+//   // to the current value of that input field.
+//   // And so with this, it is now this component.
+//   // So basically it's React who is in charge
+//   // of the state and really of the entire element.
+//   // And so that's the reason why this technique
+//   // is called controlled element.
+
+//   return (
+//     <form className="add-form" onSubmit={handleSubmit}>
+//       <h3>What do you need for your 😍 trip? </h3>
+//       <select
+//         type="number"
+//         value={quantity}
+//         onChange={(e) => setQuantity(Number(e.target.value))}
+
+//         //         So we set quantity
+//         // based on e.target.value,
+//         // and by the way, this value is coming directly
+//         // from the option so right from here.
+//         // So that's why we need to set the value
+//         // here inside of each option.
+//       >
+//         {Array.from({ length: 20 }, (_, i) => i + 1).map((num) => (
+//           <option value={num} key={num}>
+//             {num}
+//           </option>
+//         ))}
+//       </select>
+
+//       {/* And so it is this value that each time that we write something, we set as
+//       the new state of the description, all right? So just to drive this home,
+//       each time that we type here, we set the state again. So we set it to the
+//       string that is currently in this input field, which will then re-render
+//       this view. So this entire form here actually. And so then that new state
+//       of description will get placed there as the value. So we always need both
+//       the value and the change here on the input element. */}
+//       <input
+//         type="text"
+//         placeholder="Item..."
+//         value={description}
+//         onChange={(e) => setDescription(e.target.value)}
+//       ></input>
+//       <button>Add</button>
+//     </form>
+//   );
+// }
+
+// function PackingList() {
+//   return (
+//     <div className="list">
+//       <ul>
+//         {initialItems.map((item) => (
+//           <Item item={item} key={item.id} />
+//         ))}
+//       </ul>
+//     </div>
+//   );
+// }
+
+// function Item({ item }) {
+//   return (
+//     <li>
+//       <span style={item.packed ? { textDecoration: "line-through" } : {}}>
+//         {item.quantity} {item.description}
+//       </span>
+//       <button>❌</button>
+//     </li>
+//   );
+// }
+
+// function Stats() {
+//   return (
+//     <footer className="stats">
+//       <em>💼 You have X items on your list, and you already packed X (X%)</em>
+//     </footer>
+//   );
+// }
+
+//////////////////////////////////////////////////////////////
+// State vs. Props
+//////////////////////////////////////////////////////////////
+// see pdf lecture and video
+
+// A very common beginner question
+// or sometimes even an interview question is this,
+// what's the difference between state and props?
+// Well, we actually already learned almost everything
+// to answer that question,
+// but let's still make the difference between state
+// and props crystal clear in this lecture,
+// which will also serve
+// as a nice summary to this entire section.
+
+////////////////////////////////////////////////////////////////
+// What is "Thinking in React"?
+////////////////////////////////////////////////////////////////
+// see pdf lecture and video
+
+////////////////////////////////////////////////////////////////
+// Fundamentals of State Management
+////////////////////////////////////////////////////////////////
+// see pdf lecture and video
+
+///////////////////////////////////////////////////////////////
+// Thinking About State and Lifting State Up
+///////////////////////////////////////////////////////////////
+
+// And now, let's add some important state to the app
+// and then also, lift that state up.
+
+import { useState } from "react";
 
 const initialItems = [
   { id: 1, description: "Passports", quantity: 2, packed: false },
@@ -206,112 +440,19 @@ function Logo() {
 }
 
 function Form() {
-  //   So in order to implement the controlled elements technique,
-  // we follow three steps.
-  // First we create a piece of state.
-  // So let's start with that
-  // and we will start here with this actual input element.
-  // So with this text field right there.
   const [description, setDescription] = useState("");
-
   const [quantity, setQuantity] = useState(1);
 
   function handleSubmit(e) {
     e.preventDefault();
 
     if (!description) return;
-
-    // And then we also need an ID.
-    // Now we could use some library here to generate that ID
-    // but let's do it quick and dirty here, just with date.now.
-    // So that should just work here in this case.
     const newItem = { description, quantity, packed: false, id: Date.now() };
     console.log(newItem);
-    //     So with this we learned how we get now this data
-    // out of the form.
 
-    //     And now finally, usually when we submit a form
-    // then afterwards, once that submission is done
-    // the form should go back to its initial state.
-    // So let's also do that.
-    // And for that we can simply use our setter functions, right?
-    // And so that's the beauty
-    // of React being in charge of the form
-    // because now all we have to do is to update the state
-    // and then this enables React to automatically keep this state
-    // in sync with these form elements.
-    // So basically that's the whole idea
-    // of the controlled elements.
     setDescription("");
     setQuantity(1);
-
-    //     So that worked really, really nicely.
-    // So this form is now a lot more real world.
-    // It's back to its initial state.
-    // And then down here we get the data.
-    // But now what do we do with this data?
-    // So at some point we will want to actually render
-    // this new object here into the user interface.
-    // So right here into this packing list, right?
-    // So how do you think we will do that?
-    // So how can we get this new state?
-    // So basically this new object that we just created
-    // into this list.
-    // And just as a reminder, this list,
-    // well, it's even easier to see in the componentry.
-    // So we have the form, and here is the list.
-    // So we want to get the data from this form
-    // into this packing list right here.
-    // So do you think that we could do that with props?
-    // Well, not really, right?
-    // Because these are sibling components.
-    // The form is not a parent component of the packing list
-    // and therefore we cannot pass props from form
-    // into the packing list, right?
-    // So because data can only flow down the tree
-    // but not up or sideways.
-    // So that was one of the important things
-    // that we learned about props, remember that?
-    // So therefore, if we cannot use props in a simple way
-    // we need to find another solution.
-    // And so this is where we really need to start
-    // thinking more about state and state management.
-    // But since this is so important as a React developer,
-    // I created an entire separate section
-    // about thinking in React and that section is up next.
-    // So what we're gonna do now is to take a break
-    // in this application and finish this section
-    // with a brief summary and a challenge to consolidate
-    // our knowledge about how to use state in React.
-    // And then after that, we will come back
-    // here to this application
-    // and then we will really make it work.
-    // So then we will be able to pass data
-    // basically from the form into the list.
-    // So that's going to be a lot of fun.
-    // So make sure to finish this section
-    // and then right afterwards,
-    // let's keep going with this application.
   }
-
-  //   So just to quickly recap,
-  // the technique of controlled elements
-  // basically consists of three steps.
-  // So we define a piece of state, like this description here
-  // then we use that piece of state
-  // on the element that we want to control.
-  // So we basically force the element to always take the value
-  // of this state variable.
-  // And then finally, of course
-  // we need to update that state variable.
-  // And we do so here with the on change handler
-  // where we then set the description
-  // to the current value of that input field.
-  // And so with this, it is now this component.
-  // So basically it's React who is in charge
-  // of the state and really of the entire element.
-  // And so that's the reason why this technique
-  // is called controlled element.
 
   return (
     <form className="add-form" onSubmit={handleSubmit}>
@@ -320,13 +461,6 @@ function Form() {
         type="number"
         value={quantity}
         onChange={(e) => setQuantity(Number(e.target.value))}
-
-        //         So we set quantity
-        // based on e.target.value,
-        // and by the way, this value is coming directly
-        // from the option so right from here.
-        // So that's why we need to set the value
-        // here inside of each option.
       >
         {Array.from({ length: 20 }, (_, i) => i + 1).map((num) => (
           <option value={num} key={num}>
@@ -335,13 +469,6 @@ function Form() {
         ))}
       </select>
 
-      {/* And so it is this value that each time that we write something, we set as
-      the new state of the description, all right? So just to drive this home,
-      each time that we type here, we set the state again. So we set it to the
-      string that is currently in this input field, which will then re-render
-      this view. So this entire form here actually. And so then that new state
-      of description will get placed there as the value. So we always need both
-      the value and the change here on the input element. */}
       <input
         type="text"
         placeholder="Item..."
@@ -383,28 +510,3 @@ function Stats() {
     </footer>
   );
 }
-
-//////////////////////////////////////////////////////////////
-// State vs. Props
-//////////////////////////////////////////////////////////////
-// see pdf lecture and video
-
-// A very common beginner question
-// or sometimes even an interview question is this,
-// what's the difference between state and props?
-// Well, we actually already learned almost everything
-// to answer that question,
-// but let's still make the difference between state
-// and props crystal clear in this lecture,
-// which will also serve
-// as a nice summary to this entire section.
-
-////////////////////////////////////////////////////////////////
-// What is "Thinking in React"?
-////////////////////////////////////////////////////////////////
-// see pdf lecture and video
-
-////////////////////////////////////////////////////////////////
-// Fundamentals of State Management
-////////////////////////////////////////////////////////////////
-// see pdf lecture and video
