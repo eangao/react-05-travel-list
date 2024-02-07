9ae5592aebbd350ff145a620bc57c12b414a108d
elmar <elmarcera@gmail.com>
2024-02-07 11:28:42 +0800

    Moving Components Into Separate Files - start

--- src/App.js
+++ src/App.js
@@ -1406,11 +1406,209 @@
 // // in the end is what we render onto the user interface.
 // // And with this
 
-//////////////////////////////////////////////////////////////
-// Clearing the List
-//////////////////////////////////////////////////////////////
-// Let's now make our application feature complete
-// by adding a button to clear up the entire list at once.
+// //////////////////////////////////////////////////////////////
+// // Clearing the List
+// //////////////////////////////////////////////////////////////
+// // Let's now make our application feature complete
+// // by adding a button to clear up the entire list at once.
+
+// import { useState } from "react";
+
+// export default function App() {
+//   const [items, setItems] = useState([]);
+
+//   function handleClearList() {
+//     const confirmed = window.confirm(
+//       "Are you sure you want to delete all items?"
+//     );
+
+//     if (confirmed) setItems([]);
+//   }
+
+//   function handleAddItems(item) {
+//     setItems((items) => [...items, item]);
+//   }
+
+//   function handleDeleteItem(id) {
+//     setItems((items) => items.filter((item) => item.id !== id));
+
+//     console.log(id);
+//   }
+
+//   function handleToggleItem(id) {
+//     setItems((items) =>
+//       items.map((item) =>
+//         item.id === id ? { ...item, packed: !item.packed } : item
+//       )
+//     );
+//   }
+
+//   return (
+//     <div className="app">
+//       <Logo />
+//       <Form onAddItems={handleAddItems} />
+
+//       <PackingList
+//         items={items}
+//         onDeleteItem={handleDeleteItem}
+//         onToggleItem={handleToggleItem}
+//         onClearList={handleClearList}
+//       />
+//       <Stats items={items} />
+//     </div>
+//   );
+// }
+
+// function Logo() {
+//   return <h1>🌴 Far Away 💼</h1>;
+// }
+
+// function Form({ onAddItems }) {
+//   const [description, setDescription] = useState("");
+//   const [quantity, setQuantity] = useState(1);
+
+//   function handleSubmit(e) {
+//     e.preventDefault();
+
+//     if (!description) return;
+//     const newItem = { description, quantity, packed: false, id: Date.now() };
+
+//     onAddItems(newItem);
+
+//     setDescription("");
+//     setQuantity(1);
+//   }
+
+//   return (
+//     <form className="add-form" onSubmit={handleSubmit}>
+//       <h3>What do you need for your 😍 trip? </h3>
+//       <select
+//         type="number"
+//         value={quantity}
+//         onChange={(e) => setQuantity(Number(e.target.value))}
+//       >
+//         {Array.from({ length: 20 }, (_, i) => i + 1).map((num) => (
+//           <option value={num} key={num}>
+//             {num}
+//           </option>
+//         ))}
+//       </select>
+
+//       <input
+//         type="text"
+//         placeholder="Item..."
+//         value={description}
+//         onChange={(e) => setDescription(e.target.value)}
+//       ></input>
+//       <button>Add</button>
+//     </form>
+//   );
+// }
+
+// function PackingList({ items, onDeleteItem, onToggleItem, onClearList }) {
+//   const [sortBy, setSortBy] = useState("input");
+//   let sortedItems;
+
+//   if (sortBy === "input") sortedItems = items;
+
+//   if (sortBy === "description")
+//     sortedItems = items
+//       .slice()
+//       .sort((a, b) => a.description.localeCompare(b.description));
+
+//   if (sortBy === "packed")
+//     sortedItems = items
+//       .slice()
+//       .sort((a, b) => Number(a.packed) - Number(b.packed));
+
+//   return (
+//     <div className="list">
+//       <ul>
+//         {sortedItems.map((item) => (
+//           <Item
+//             item={item}
+//             onDeleteItem={onDeleteItem}
+//             onToggleItem={onToggleItem}
+//             key={item.id}
+//           />
+//         ))}
+//       </ul>
+//       <div className="actions">
+//         <select value={sortBy} onChange={(e) => setSortBy(e.target.value)}>
+//           <option value="input">Sort by input order</option>
+//           <option value="description">Sort by description</option>
+//           <option value="packed">Sort by packed status</option>
+//         </select>
+//         <button onClick={onClearList}>Clear list</button>
+//       </div>
+//     </div>
+//   );
+// }
+
+// function Item({ item, onDeleteItem, onToggleItem }) {
+//   return (
+//     <li>
+//       <input
+//         type="checkbox"
+//         value={item.packed}
+//         onChange={() => onToggleItem(item.id)}
+//       />
+//       <span style={item.packed ? { textDecoration: "line-through" } : {}}>
+//         {item.quantity} {item.description}
+//       </span>
+//       <button onClick={() => onDeleteItem(item.id)}>❌</button>
+//     </li>
+//   );
+// }
+
+// function Stats({ items }) {
+//   if (!items.length)
+//     return (
+//       <p className="stats">
+//         <em>Start adding some items to your packing list 🚀</em>
+//       </p>
+//     );
+
+//   const numItems = items.length;
+//   const numPacked = items.filter((item) => item.packed).length;
+//   const percentage = Math.round((numPacked / numItems) * 100);
+
+//   return (
+//     <footer className="stats">
+//       <em>
+//         {percentage === 100
+//           ? "You got everything! Ready to go ✈"
+//           : `💼 You have ${numItems} items on your list, and you already packed
+//         ${numPacked} (${percentage}%)`}
+//       </em>
+//     </footer>
+//   );
+// }
+
+// // Now maybe you got a little bit annoyed
+// // at all the scrolling that we had to do here now lately
+// // because our components were getting bigger and bigger.
+// // So whenever we wanted like to pass in something here
+// // then we had to scroll all the way down here
+// // to then accept, for example, these props right here.
+// // And so that's why I said
+// // in the very beginning
+// // that in real world applications
+// // we usually have one component per file.
+// // And so in the next lecture I will show you a trick
+// // of how we can basically divide this one file
+// // into multiple files.
+// // So one file per component.
+
+////////////////////////////////////////////////////////////////
+// Moving Components Into Separate Files
+////////////////////////////////////////////////////////////////
+
+// So let's now split up our App.js file
+// into multiple component files.
+// And this will just be a very simple exercise
+// of taking each of these components
+// and placing them into their own file.
 
 import { useState } from "react";
 
@@ -1584,18 +1782,3 @@ function Stats({ items }) {
     </footer>
   );
 }
-
-// Now maybe you got a little bit annoyed
-// at all the scrolling that we had to do here now lately
-// because our components were getting bigger and bigger.
-// So whenever we wanted like to pass in something here
-// then we had to scroll all the way down here
-// to then accept, for example, these props right here.
-// And so that's why I said
-// in the very beginning
-// that in real world applications
-// we usually have one component per file.
-// And so in the next lecture I will show you a trick
-// of how we can basically divide this one file
-// into multiple files.
-// So one file per component.
