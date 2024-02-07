2ac03ab32b2d4baecbcb512b9ce65140daf058b8
elmar <elmarcera@gmail.com>
2024-02-07 10:34:40 +0800

    Sorting Items - start

--- src/App.js
+++ src/App.js
@@ -1032,22 +1032,175 @@
 // was derived state.
 // Sounds complicated but it's actually pretty straightforward.
 
-////////////////////////////////////////////////////////////////////
-// Calculating Statistics as Derived State
-////////////////////////////////////////////////////////////////////
+// ////////////////////////////////////////////////////////////////////
+// // Calculating Statistics as Derived State
+// ////////////////////////////////////////////////////////////////////
 
-// Of course, let's now use the idea
-// of derived state in practice.
-// And in particular
-// we now want to calculate our statistics here.
-// So calculating the number of items on the list,
-// how many we already have packed
-// and then the percentage of that.
-// Now, if we think about these numbers,
-// for example, the number of items in the list,
-// that number can be directly computed
-// from the items array itself, right?
-// And so derived state is perfect for this,
+// // Of course, let's now use the idea
+// // of derived state in practice.
+// // And in particular
+// // we now want to calculate our statistics here.
+// // So calculating the number of items on the list,
+// // how many we already have packed
+// // and then the percentage of that.
+// // Now, if we think about these numbers,
+// // for example, the number of items in the list,
+// // that number can be directly computed
+// // from the items array itself, right?
+// // And so derived state is perfect for this,
+
+// import { useState } from "react";
+
+// export default function App() {
+//   const [items, setItems] = useState([]);
+
+//   function handleAddItems(item) {
+//     setItems((items) => [...items, item]);
+//   }
+
+//   function handleDeleteItem(id) {
+//     setItems((items) => items.filter((item) => item.id !== id));
+
+//     console.log(id);
+//   }
+
+//   function handleToggleItem(id) {
+//     setItems((items) =>
+//       items.map((item) =>
+//         item.id === id ? { ...item, packed: !item.packed } : item
+//       )
+//     );
+//   }
+
+//   return (
+//     <div className="app">
+//       <Logo />
+//       <Form onAddItems={handleAddItems} />
+
+//       <PackingList
+//         items={items}
+//         onDeleteItem={handleDeleteItem}
+//         onToggleItem={handleToggleItem}
+//       />
+//       <Stats items={items} />
+//     </div>
+//   );
+// }
+
+// function Logo() {
+//   return <h1>🌴 Far Away 💼</h1>;
+// }
+
+// function Form({ onAddItems }) {
+//   const [description, setDescription] = useState("");
+//   const [quantity, setQuantity] = useState(1);
+
+//   function handleSubmit(e) {
+//     e.preventDefault();
+
+//     if (!description) return;
+//     const newItem = { description, quantity, packed: false, id: Date.now() };
+
+//     onAddItems(newItem);
+
+//     setDescription("");
+//     setQuantity(1);
+//   }
+
+//   return (
+//     <form className="add-form" onSubmit={handleSubmit}>
+//       <h3>What do you need for your 😍 trip? </h3>
+//       <select
+//         type="number"
+//         value={quantity}
+//         onChange={(e) => setQuantity(Number(e.target.value))}
+//       >
+//         {Array.from({ length: 20 }, (_, i) => i + 1).map((num) => (
+//           <option value={num} key={num}>
+//             {num}
+//           </option>
+//         ))}
+//       </select>
+
+//       <input
+//         type="text"
+//         placeholder="Item..."
+//         value={description}
+//         onChange={(e) => setDescription(e.target.value)}
+//       ></input>
+//       <button>Add</button>
+//     </form>
+//   );
+// }
+
+// function PackingList({ items, onDeleteItem, onToggleItem }) {
+//   return (
+//     <div className="list">
+//       <ul>
+//         {items.map((item) => (
+//           <Item
+//             item={item}
+//             onDeleteItem={onDeleteItem}
+//             onToggleItem={onToggleItem}
+//             key={item.id}
+//           />
+//         ))}
+//       </ul>
+//     </div>
+//   );
+// }
+
+// function Item({ item, onDeleteItem, onToggleItem }) {
+//   return (
+//     <li>
+//       <input
+//         type="checkbox"
+//         value={item.packed}
+//         onChange={() => onToggleItem(item.id)}
+//       />
+//       <span style={item.packed ? { textDecoration: "line-through" } : {}}>
+//         {item.quantity} {item.description}
+//       </span>
+//       <button onClick={() => onDeleteItem(item.id)}>❌</button>
+//     </li>
+//   );
+// }
+
+// function Stats({ items }) {
+//   if (!items.length)
+//     return (
+//       <p className="stats">
+//         <em>Start adding some items to your packing list 🚀</em>
+//       </p>
+//     );
+
+//   const numItems = items.length;
+//   const numPacked = items.filter((item) => item.packed).length;
+//   const percentage = Math.round((numPacked / numItems) * 100);
+
+//   return (
+//     <footer className="stats">
+//       <em>
+//         {percentage === 100
+//           ? "You got everything! Ready to go ✈"
+//           : `💼 You have ${numItems} items on your list, and you already packed
+//         ${numPacked} (${percentage}%)`}
+//       </em>
+//     </footer>
+//   );
+// }
+
+/////////////////////////////////////////////////////////////////////////
+// Sorting Items
+/////////////////////////////////////////////////////////////////////////
+// Let's add a new feature to our application
+// which is to allow users to sort the items
+// by three different criteria.
+// So basically we will build this select box criteria
+// and then from there the user can choose which
+// of these criteria they want to sort the list by.
+// So something that's very common in most web applications.
+// And so let's now build a very simple version of that.
 
 import { useState } from "react";
 
